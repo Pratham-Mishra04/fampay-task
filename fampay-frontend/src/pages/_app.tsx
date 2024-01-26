8af578f8f3037fc4b898660da1d27ad6454af8fd
Pratham-Mishra04 <prathamm0409@gmail.com>
2024-01-26 11:38:00 +0530

    feat: frontend configured

--- fampay-frontend/src/pages/_app.tsx
+++ fampay-frontend/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
+import '@/styles/extras.tailwind.css';
 import type { AppProps } from "next/app";
+import { Inter } from 'next/font/google';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--inter-font',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return    <main className={`${inter.variable}`}>
+  <ToastContainer />
+  <Component {...pageProps} />
+</main>
 }
--- fampay-frontend/tailwind.config.ts
+++ fampay-frontend/tailwind.config.ts
@@ -8,7 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-
+      fontFamily: {
+        primary: ['var(--inter-font)'],
+      },
+      colors: {
+        primary_black: '#2e2c2c',
+      },
+      animation: {
+        fade_third: 'fade 0.3s ease-in-out',
+        fade_half: 'fade 0.5s ease-in-out',
+        fade_1: 'fade 1s ease-in-out',
+        fade_2: 'fade 2s ease-in-out',
+      },
+      keyframes: {
+        fade: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
